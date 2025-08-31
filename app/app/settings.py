7614aaa6ec931119bcdff075c8e5c35f85e1a3dc
farzan alaei <farzan.70@gmail.com>
2025-08-31 20:20:17 +0330

    fix choose file in spectacular with delete line code in settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-from drf_spectacular.settings import SPECTACULAR_DEFAULTS
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
